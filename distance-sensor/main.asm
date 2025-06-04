871fb3c4fdbcb8146f4e41d3d77c494fd5618d77
BenediktNau <bene.nau27@gmail.com>
2025-06-04 13:08:39 +0200

    Functional but not Stable

--- distance-sensor/main.asm
+++ distance-sensor/main.asm
@@ -5,28 +5,57 @@
 ; Author : ESLab_NetLab-Student
 ;
 
-.equ trigPin = 2
-.equ inputPin = 3
 
-	sbi DDRD , trigPin
 
-start :
-	cbi	PIND, trigPin
-	rcall Delay_2microsec
-	sbi PIND , trigPin 
-	rcall Delay_10microsec
-	cbi PIND, trigPin
-	LDI r20 , 1
-	
 
-	rjmp start
+.equ trigPin= 2
+.equ echoPin= 3
+
+    sbi DDRD, trigPin      ; TRIG = output
+    cbi DDRD, echoPin  
+
+start:
+	cbi PORTD, trigPin
+    rcall Delay_2microsec
+    sbi PORTD, trigPin
+    rcall Delay_10microsec
+    cbi PORTD, trigPin
+    
+
+   
+wait_echo_high:
+    sbis PIND, echoPin    
+    rjmp wait_echo_high
+
+    
+    ldi r16, 0
+    sts TCNT1H, r16
+    sts TCNT1L, r16
+
+    
+    ldi r16, (1 << 0)      
+    sts TCCR1B, r16
+
+
+wait_echo_low:
+    sbic PIND, echoPin     
+    rjmp wait_echo_low
+
+    ldi r16, 0
+    sts TCCR1B, r16
+
+    lds r18, TCNT1L 
+    lds r19, TCNT1H
+
+
+    rjmp start
 
 
 Delay_10microsec:                
 Delay1_10:
     LDI     r16,   10     ; One clock cycle
 Delay2_10:
-    LDI     r17,   4     ; One clock cycle
+    LDI     r17,   6     ; One clock cycle
 Delay3_10:
     DEC     r17            ; One clock cycle
     NOP                     ; One clock cycle
